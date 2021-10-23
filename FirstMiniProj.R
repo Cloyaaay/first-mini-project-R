9add948a070b0a1f6003ae127d014ff96af8b386
CASuyao <suayocloei@gmail.com>
2021-10-24 01:34:35 +0800

    Add instructions how to run code for problem 1

--- FirstMiniProj.R
+++ FirstMiniProj.R
@@ -10,3 +10,11 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   mean_dt <- mean(values, na.rm=T) #calculate mean using all values of pollutant from the vector
   return(mean_dt)
 }
+
+#How to run:
+#Load the source file using the command, source(FirstMiniProj.R)
+#Run the FirstMiniProj.R script in RStudio
+#Run the ff commands in the console in RStudio to check for the outputs
+#pollutantmean("insert your path here", "sulfate", 1:10)
+#pollutantmean("insert your path here", "nitrate", 70:72)
+#pollutantmean("insert your path here", "nitrate", 23)
